bdb9192a4c2be4a2b5cf10491c0d94a3e2b34e03
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-12-26 01:35:33 +0200

    Adding commandLineRunner, specifying port

--- src/main/java/ItCareerElevatorFifthExercise/DTOs/UserLocationDTO.java
+++ src/main/java/ItCareerElevatorFifthExercise/DTOs/UserLocationDTO.java
@@ -3,9 +3,11 @@ package ItCareerElevatorFifthExercise.DTOs;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 public class UserLocationDTO {
 
